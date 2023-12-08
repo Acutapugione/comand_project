8b49fac3a73682eb7e27d027f3058510ac52b174
danilaleks <127975163+danilaleks@users.noreply.github.com>
2023-12-08 17:12:00 +0200

    Update command.py

--- Project/basedata/command.py
+++ Project/basedata/command.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Table
-from sqlalchemy.orm import relationship, mapped_column, Mapped
+from sqlalchemy.orm import relationship, mapped_column, Mapped,Staff
 from datetime import datetime
 from . import Base
 from dataclasses import dataclass
@@ -10,17 +10,8 @@ class Command(Base):
     __tablename__ = 'commands'
 
     id:  Mapped[int] = mapped_column(primary_key=True)
-    text: Mapped[str] = mapped_column(String(225)
+    text: Mapped[str] = mapped_column(String(225))
     timestamp: Mapped[str] = mapped_column(DateTime, default=datetime.utcnow)
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id')
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     
     user: Mapped["Staff"] = relationship('User', back_populates='commands')
-
-# commands_table = Table(
-#     'commands',
-#     metadata,
-#     Column('id', Integer, primary_key=True),
-#     Column('text', String(225)),
-#     Column('timestamp', DateTime, default=datetime.utcnow),
-#     Column('user_id', Integer, ForeignKey('users.id')),
-# )
