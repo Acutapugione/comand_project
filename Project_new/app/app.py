9867a0dc52df8988b01fa5cccf1325054b0c26d2
danilaleks <127975163+danilaleks@users.noreply.github.com>
2024-01-09 21:43:46 +0200

    Update app.py

--- Project_new/app/app.py
+++ Project_new/app/app.py
@@ -1,29 +1,36 @@
-import openmeteo_requests
-import requests_cache
-import pandas as pd
-from retry_requests import retry
+from fastapi import FastAPI
+from models import PrognosForecastDays, PrognosToday, PrognosTodayId
+from database import SessionLocal, engine
 from sqlalchemy.orm import sessionmaker
-from models import WeatherData
-from database import engine
-from datetime import datetime
-from database import SessionLocal
 
-cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
-retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-openmeteo = openmeteo_requests.Client(session=retry_session)
+app = FastAPI()
 
-async def get_and_store_weather_data(request):
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-    db = SessionLocal()
+def get_db():
+    db = Session() 
     try:
-        for _, row in hourly_dataframe.iterrows():
-            weather_data = WeatherData(
-                date=row['date'],
-                temperature_2m=row['temperature_2m'],
-                relative_humidity_2m=row['relative_humidity_2m'],
-                pressure_msl=row['pressure_msl']
-            )
-            db.add(weather_data)
-        db.commit()
+        yield db
     finally:
         db.close()
+
+@app.post("/prognos_forecast_days/")
+async def create_prognos_forecast_days(prognos_forecast_days: PrognosForecastDays, db: Session = get_db()):
+    db.add(prognos_forecast_days)
+    db.commit()
+    db.refresh(prognos_forecast_days)
+    return prognos_forecast_days
+
+@app.post("/prognos_today/")
+async def create_prognos_today(prognos_today: PrognosToday, db: Session = get_db()):
+    db.add(prognos_today)
+    db.commit()
+    db.refresh(prognos_today)
+    return prognos_today
+
+@app.post("/prognos_today_id/")
+async def create_prognos_today_id(prognos_today_id: PrognosTodayId, db: Session = get_db()):
+    db.add(prognos_today_id)
+    db.commit()
+    db.refresh(prognos_today_id)
+    return prognos_today_id
