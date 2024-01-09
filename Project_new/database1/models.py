2dc3398a5d3b09d28be9ce33f6f595e79a69841e
danilaleks <127975163+danilaleks@users.noreply.github.com>
2024-01-09 21:42:30 +0200

    Update models.py

--- Project_new/database1/models.py
+++ Project_new/database1/models.py
@@ -1,12 +1,42 @@
-from sqlalchemy import Column, Integer, Float, DateTime
-from database import Base
+from pydantic import BaseModel
 from datetime import datetime
+from typing import List
+from sqlalchemy import Column, Integer, Float, DateTime, String
+from database import Base
+from sqlalchemy.orm import Mapped,mapped_column
 
-class WeatherData(Base):
-    __tablename__ = 'weather_data'
+class PrognosForecastDays(Base):
+    __tablename__ = 'prognos_forecast_days'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id: Mapped[Integer] = mapped_column(primary_key=True)
+    city: Mapped[str] 
     date = Column(DateTime, default=datetime.utcnow)
-    temperature_2m = Column(Float)
-    relative_humidity_2m = Column(Float)
-    pressure_msl = Column(Float)
+    temperature_min: Column[Float]
+    temperature_max: Column[Float]
+    wind_speed: Column[Float]
+    wind_direction: Column[Float]
+    pressure: Column[Float]
+
+class PrognosToday(Base):
+    __tablename__ = 'prognos_today'
+
+    id: Mapped[Integer] = mapped_column(primary_key=True)
+    city: Column[String]
+    country: Column[String]
+    weather_description: Column[String]
+    temperature: Column[Float]
+    wind_speed: Column[Float]
+    wind_direction: Column[String]
+    pressure: Column[Float]
+    timestamp: Column [DateTime] = Mapped (default=datetime.utcnow)
+
+class PrognosTodayId(Base):
+    __tablename__ = 'prognos_today_id'
+    id: [Integer] = mapped_column(primary_key=True)
+    city: Column[String]
+    country: Column[String]
+    datetime = Column(DateTime, default=datetime.utcnow)
+    temperature: Column[Float]
+    wind_speed : Column[Float]
+    wind_direction: Column[String]
+    pressure: Column[Float]
